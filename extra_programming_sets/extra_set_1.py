4f2020b4b08d6df1c13b2598889d97f4513929c0
Joe Ilagan <joe.s.ilagan@gmail.com>
2025-07-04 12:09:11 +0800

    added extra programming sets

---
+++ extra_programming_sets/extra_set_1.py
@@ -0,0 +1,13 @@
+def bin_to_dec(binary_string):
+	"""
+	Given a string that represents a binary number, convert it to the decimal number it represents.
+
+	Do not use the built-in binary to decimal converter in Python. 
+	"""
+
+def dec_to_bin(number):
+	"""
+	Given an int, convert it to a string that represents the number in binary notation.
+
+	Do not use the built-in decimal to binary converter in Python.
+	"""
